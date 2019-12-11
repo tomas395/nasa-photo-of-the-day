f0f3bb1be9e53f9b1986531a11197310626e82e5
Tomas Ortiz <TomasOrtiz@protonmail.com>
2019-12-11 15:39:54 -0800

    got data to be fetched

---
+++ components/ApodCard.js
@@ -0,0 +1,9 @@
+// import React from "react";
+
+// const ApodCard = props => {
+//   return( <div>
+//   </div>;
+//   )
+// };
+
+// export default ApodCard;
--- src/App.js
+++ src/App.js
@@ -3,6 +3,21 @@ import "./App.css";
 import axios from "axios";
 
 function App() {
+  const [pod, setApod] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(
+        "https://api.nasa.gov/planetary/apod?api_key=hofCICR2ekMLxN3GiGmInR1tyruck8ehWH4bjU8E"
+      )
+      .then(res => {
+        console.log("Fetched!", res);
+        setApod(res.data);
+      })
+      .catch(err => {
+        console.log("Error: The data was not returned!", err);
+      });
+  }, []);
   return (
     <div className="App">
       <p>
@@ -14,3 +29,22 @@ function App() {
 }
 
 export default App;
+
+// import React from "react";
+// import ApodDisplay from "./components/ApodDisplay";
+// import "./App.css";
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <p>
+//         Read through the instructions in the README.md file to build your NASA
+//         app! Have fun 🚀!
+//       </p>
+//       <h1>Astronomy Picture of the Day courtesy of NASA!</h1>
+//       <ApodDisplay />
+//     </div>
+//   );
+// }
+
+// export default App;
